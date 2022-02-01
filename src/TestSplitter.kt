024517e2ca6eed8df920c8a207c23765fa62528d
aNOOBis666 <mr.zooko@mail.ru>
2022-02-01 07:20:48 +0300

    IntConst created

--- src/TestSplitter.kt
+++ src/TestSplitter.kt
@@ -4,12 +4,12 @@ import solution.splitMessageExtension
 fun main() {
 	val outPutShort = StringConst.SHORT_MESSAGE.value.splitMessageExtension(maxLengthMessage = 14)
 	println("result: $outPutShort")
-//      result: [Lorem]
+//        result: [Lorem]
 	val outPutMiddle = StringConst.MIDDLE_MESSAGE.value.splitMessageExtension(maxLengthMessage = 14)
 	println("result: $outPutMiddle")
-//      result: [Lorem 1/3, ipsum 2/3, dolor 3/3]
+//        result: [Lorem 1/3, ipsum 2/3, dolor 3/3]
 	val outPutLong = StringConst.LONG_MESSAGE.value.splitMessageExtension(maxLengthMessage = 50)
 	println("result: $outPutLong")
-//      result: [Lorem ipsum dolor sit amet, consectetur adipiscing 1/5, elit, sed do eiusmod tempor incididunt ut labore et 2/5, dolore magna aliqua. Ut enim ad minim veniam, quis 3/5, nostrud exercitation ullamco laborious nisi ut 4/5, aliquip ex ea commodo consequat. 5/5]
+//        result: [Lorem ipsum dolor sit amet, consectetur 1/10, adipiscing elit, sed do eiusmod tempor 2/10, incididunt ut labore et dolore 3/10, magna aliqua. Ut enim ad 4/10, minim veniam, quis 5/10, nostrud exercitation 6/10, ullamco laborious 7/10, nisi ut aliquip 8/10, ex ea commodo 9/10, consequat. 10/10]
 
 }
---
+++ src/solution/IntConst.kt
@@ -0,0 +1,6 @@
+package solution
+
+enum class IntConst(val index: Int){
+	SPECIAL_SYMBOLS(2),
+	LONELY_MESSAGE(1)
+}
