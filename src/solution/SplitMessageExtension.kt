d404c96ac9f348bf403c00b8d2953720a98e99d2
aNOOBis666 <mr.zooko@mail.ru>
2022-02-01 07:17:28 +0300

    Extension fun reworked

--- src/solution/SplitMessageExtension.kt
+++ src/solution/SplitMessageExtension.kt
@@ -7,7 +7,6 @@ import kotlin.math.ceil
 Необходимо разбить входную строку на смс-ки
 То есть написать метод, который принимает строку и максимальную длину смс, а возвращает массив строк сформированный
 по следующим правилам:
-
 1) разбивать текст можно только по пробелам
 2) если получилось >1 смс-ки то нужно добавить ко всем суффикс вида " 12/123" где 12 номер текущей смс 123 общее число смс
 3) длина смс включая суффикс должна быть не больше переданного лимита
@@ -16,40 +15,45 @@ import kotlin.math.ceil
 лимита и прочее)
  */
 
+private var numberOfMessages: Int = 2
+private val globalText = mutableListOf<String>()
+
 fun String.splitMessageExtension(maxLengthMessage: Int): List<String> {
-    val resultList = mutableListOf<String>()
-    val message = mutableListOf<String>()
-    var wordIndex = 1
-    var messageIndex = 1
-    var lengthMessage = 0
+	if (this.isEmpty()) throw NullPointerException(StringConst.NULL_POINTER_HANDLER.value)
 
-    if (this.isEmpty()) throw NullPointerException(StringConst.NULL_POINTER_HANDLER.value)
+	numberOfMessages = ceil((this.length.toDouble() / maxLengthMessage.toDouble())).toInt()
+	if (numberOfMessages == IntConst.LONELY_MESSAGE.index) {
+		return listOf(this)
+	}
+	val splitText: List<String> = this.split(StringConst.EMPTY_SEPARATOR.value)
+	return updateNumberOfMessages(splitText, maxLengthMessage)
+}
 
-    var numberOfMessages = ceil((this.length.toDouble() / maxLengthMessage.toDouble())).toInt()
-    if (numberOfMessages != 1) numberOfMessages =
-        ceil(((this.length + 2 * numberOfMessages).toDouble() / maxLengthMessage.toDouble())).toInt()
+fun updateNumberOfMessages(splitText: List<String>, maxLengthMessage: Int): List<String> {
+	val localText = mutableListOf<String>()
+	var localLength = 0
+	var numOfMessageCounter = 1
+	for ((index, word) in splitText.withIndex()) {
+		try {
+			localText.add(word)
+			localLength += word.length
 
-    val splitText: List<String> = this.split(" ")
-
-    for (value in splitText) {
-        try {
-            message.add(value)
-            lengthMessage += value.length
-            if ((
-                        lengthMessage + splitText[wordIndex].length + wordIndex.toString().length + 2 +
-                        numberOfMessages.toString().length) > maxLengthMessage
-            ) {
-                resultList.add(message.joinToString(" ") + " ${messageIndex}/$numberOfMessages")
-                messageIndex++
-                message.clear()
-                lengthMessage = 0
-            }
-            wordIndex++
-        } catch (e: RuntimeException) {
-            if (numberOfMessages != 1) resultList.add(message.joinToString(" ") + " ${messageIndex}/$numberOfMessages")
-            else resultList.add(message.joinToString(" "))
-            message.clear()
-        }
-    }
-    return resultList.toList()
+			if ((localLength + splitText[index + 1].length + index + 1 + IntConst.SPECIAL_SYMBOLS.index + numberOfMessages.toString().length) >= maxLengthMessage) {
+				globalText.add(localText.joinToString(StringConst.EMPTY_SEPARATOR.value) + " ${numOfMessageCounter}/$numberOfMessages")
+				localText.clear()
+				localLength = 0
+				numOfMessageCounter++
+			}
+		} catch (e: Exception) {
+			globalText.add(localText.joinToString(StringConst.EMPTY_SEPARATOR.value) + " ${numOfMessageCounter}/$numberOfMessages")
+			localText.clear()
+			localLength = 0
+		}
+	}
+	if (globalText.size != numberOfMessages) {
+		numberOfMessages++
+		globalText.clear()
+		updateNumberOfMessages(splitText, maxLengthMessage)
+	}
+	return globalText
 }
--- src/solution/StringConst.kt
+++ src/solution/StringConst.kt
@@ -1,8 +1,9 @@
 package solution
 
 enum class StringConst(val value: String) {
-    SHORT_MESSAGE("Lorem"),
-    MIDDLE_MESSAGE("Lorem ipsum dolor"),
-    LONG_MESSAGE("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborious nisi ut aliquip ex ea commodo consequat."),
-    NULL_POINTER_HANDLER("Current meaning is null"),
+	SHORT_MESSAGE("Lorem"),
+	MIDDLE_MESSAGE("Lorem ipsum dolor"),
+	LONG_MESSAGE("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborious nisi ut aliquip ex ea commodo consequat."),
+	NULL_POINTER_HANDLER("Current meaning is null"),
+	EMPTY_SEPARATOR(" ")
 }
